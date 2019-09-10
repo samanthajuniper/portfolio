e013eb21c4ad24fee7fd3d89a9ecddd897966ce9
samanthajuniper <samo5532@colorado.edu>
2019-09-10 08:22:53 -0600

    fixed logos to match branding guides

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,17 +13,17 @@ class Navbar extends Component {
                     <NavLink to={pdf} target="_blank" className="is-size-5 has-text-weight-bold page-link ">Resume</NavLink>
                 </div>
                 <div className="icon-container is-flex">
-                    <a href=""  class="social-icon">
+                    <a href="https://github.com/samanthajuniper/" target="_blank" rel="noopener noreferrer" className="social-icon github">
                         <span className="icon is-large">
                             <i className="fab fa-github-square is-size-3"></i>
                         </span>
                     </a>
-                    <a href="" target="_blank" class="social-icon">
+                    <a title="https://linkedin.com/in/samantha-morgan-a63306146" href="https://linkedin.com/in/samantha-morgan-a63306146" target="_blank" rel="noopener noreferrer" className="social-icon linkedin">
                         <span className="icon is-large">
                             <i className="fab fa-linkedin is-size-3"></i>
                         </span>
                     </a>
-                    <a href="" target="_blank" class="social-icon">
+                    <a href="mailto: samanthajmorgan36@gmail.com" target="_blank" rel="noopener noreferrer" title="email" className="social-icon mail">
                         <span className="icon is-large">
                             <i className="fas fa-paper-plane is-size-3"></i>
                         </span>
