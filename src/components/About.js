7e0e53cc8d407e0bfdae76985c11c2f2bd67ff74
samanthajuniper <samo5532@colorado.edu>
2019-09-17 11:03:14 -0600

    Working out responsive styling for the header and navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Leaf from './Leaf'
 import Navbar from './Navbar';
-const logo = require('../assets/introImage.png');
 
 
 class About extends Component {
@@ -23,25 +22,24 @@ class About extends Component {
             }, 2000)
     };
 
-      componentDidMount() {
+    componentDidMount() {
         this.showtagline();
     }
-
 
     render() {
 
         return (
-          <div id="about">
-            <div className="header-wrapper is-flex">
-                <div className="title-div is-flex">
-                    <div className="name title"><h1>I'm Sam</h1></div>
-                    <div className="ellipsis"></div>
-                </div>
-                <div className="tagline-div">
-                    <h1 id="tagline" className={this.state.class}>{this.state.tagline}</h1>
+            <div id="about">
+                <div className="header-wrapper is-flex">
+                    <div className="title-div is-flex">
+                        <div className="name title"><h1>I'm Sam</h1></div>
+                        <div className="ellipsis" />
+                    </div>
+                    <div className="tagline-div">
+                        <h1 id="tagline" className={this.state.class}>{this.state.tagline}</h1>
+                    </div>
                 </div>
             </div>
-        </div>
         );
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,25 +7,25 @@ class Navbar extends Component {
     render() {
         return(
             <div id="navbar">
-                <div className="link-container is-flex">
-                    <NavLink to="/" className="is-size-5 has-text-weight-bold page-link ">About</NavLink>
-                    <NavLink to="/projects" className="is-size-5 has-text-weight-bold page-link ">Projects</NavLink>
-                    <NavLink to={pdf} target="_blank" className="is-size-5 has-text-weight-bold page-link ">Resume</NavLink>
+                <div className="link-container  is-flex">
+                    <NavLink to="/" className="is-size-5 is-size-6-mobile has-text-weight-bold page-link">About</NavLink>
+                    <NavLink to="/projects" className="is-size-5 is-size-6-mobile has-text-weight-bold page-link">Projects</NavLink>
+                    <NavLink to={pdf} target="_blank" className="is-size-5 is-size-6-mobile has-text-weight-bold page-link">Resume</NavLink>
                 </div>
                 <div className="icon-container is-flex">
                     <a href="https://github.com/samanthajuniper/" target="_blank" rel="noopener noreferrer" className="social-icon github">
                         <span className="icon is-large">
-                            <i className="fab fa-github-square is-size-3"></i>
+                            <i className="fab fa-github-square is-size-3 is-size-4-mobile"></i>
                         </span>
                     </a>
                     <a title="https://linkedin.com/in/samantha-morgan-a63306146" href="https://linkedin.com/in/samantha-morgan-a63306146" target="_blank" rel="noopener noreferrer" className="social-icon linkedin">
                         <span className="icon is-large">
-                            <i className="fab fa-linkedin is-size-3"></i>
+                            <i className="fab fa-linkedin is-size-3 is-size-4-mobile"></i>
                         </span>
                     </a>
                     <a href="mailto: samanthajmorgan36@gmail.com" target="_blank" rel="noopener noreferrer" title="email" className="social-icon mail">
                         <span className="icon is-large">
-                            <i className="fas fa-paper-plane is-size-3"></i>
+                            <i className="fas fa-paper-plane is-size-3 is-size-4-mobile"></i>
                         </span>
                     </a>
                 </div>
